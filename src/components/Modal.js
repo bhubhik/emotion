32bdb2a6308ca6e7a85ddef10c7fa21c5ce36054
bhubhik <bhubhik@gmail.com>
2023-01-28 10:21:53 +1100

    without emotion

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Modal = () => {
   return (
     <div>
-      <p>Are you sure you want to learn Emotion CSS?</p>
+      <p>Are you enjoying learning Emotion CSS?</p>
       <div>
         <button>No</button>
-        <button>Yes, I'd love to!</button>
+        <button>Yes, I love it!</button>
       </div>
     </div>
   );
